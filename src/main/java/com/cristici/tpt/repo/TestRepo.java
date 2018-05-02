d063ae381af904c3eab4d09fd6898b876365ad36
cristici <cristian.cioriia@cegeka.com>
2018-05-02 18:13:25 +0300

    Add Repository annotation.

--- src/main/java/com/cristici/tpt/repo/TestRepo.java
+++ src/main/java/com/cristici/tpt/repo/TestRepo.java
@@ -2,6 +2,8 @@ package com.cristici.tpt.repo;
 
 import com.cristici.tpt.domain.CommercialData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TestRepo extends JpaRepository<CommercialData, Long> {
 }
